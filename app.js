a3770f49c52b07830440cf5bea9f20e67f164bdd
Darshan K <darshanhardy123@gmail.com>
2025-06-12 09:20:14 +0530

    Added JavascriptAPI and some dom manipulation

--- app.js
+++ app.js
@@ -1,21 +1,34 @@
 //promises
 
-// let promises = new Promise((resolve,reject)=>{
+// const promises = ()=>
+//     {
+//     return new Promise((resolve,reject)=>{
 //     console.log("Promise executed.");
+//     //resolve("Success");
+//     reject("Rejected");
+// });
+// };
+
+// let newPromise = promises();
+// newPromise.then(()=>{
+//     console.log("Fulfilles");
+// });
+// newPromise.catch(()=>{
+//     console.log("Error:");
 // });
 
-function getData(dataId,getNextData){
-    return new Promise((resolve,reject)=>
-    {
-        setTimeout(()=>{console.log(dataId);
-            resolve("Success");
-            if(getNextData)
-            {
-            getNextData();
-            }
-        },10000);
-    });
-}
+// function getData(dataId,getNextData){
+//     return new Promise((resolve,reject)=>
+//     {
+//         setTimeout(()=>{console.log(dataId);
+//             resolve("Success");
+//             if(getNextData)
+//             {
+//             getNextData();
+//             }
+//         },10000);
+//     });
+// }
 
 
 // function getData(dataId,getNextData)
@@ -31,3 +44,41 @@ function getData(dataId,getNextData){
 // }
 // getData(1,()=>{getData(2);});
 // getData(3,()=>{getData(4);});
+
+const url = "https://meowfacts.herokuapp.com/";
+const fact=document.getElementById("Fact");
+const button=document.getElementById("getFact");
+const para1=document.createElement("p");
+para1.setAttribute("id","para1");
+
+button.addEventListener("click",getFact);
+async function getFact() {
+
+    try {
+    let para2=document.getElementById("para2");
+    if(para2)
+    {
+        para2.remove();
+    }
+    fact.prepend(para1);
+    const response = await fetch(url);
+    const data = await response.json();
+    // fact.innerText=data.data[0];
+    const text=data.data[0];
+    // fact.innerText=text;
+    para1.innerText="Loading....";
+    setTimeout(()=>{
+        fact.removeChild(para1);
+        let para2=document.createElement("p");
+        para2.setAttribute("id","para2");
+        para2.innerText=text;
+        fact.appendChild(para2);
+    },2000);
+    
+    console.log(data.data[0]); 
+    }
+    catch(error)
+    {
+     console.log(error);
+    }
+}
